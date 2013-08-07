c38a787976f3f5cd233394962fce612ed7fe7c6b
Sujit Roy <la_costa_vonita@yahoo.com>
2013-08-07 14:10:22 +0530

    Updating the Sherpa Delete script with modular design and generic use cases

--- SherpaDelete/src/main/java/com/sujit/bhejafry/sherpadelete/App.java
+++ SherpaDelete/src/main/java/com/sujit/bhejafry/sherpadelete/App.java
@@ -19,28 +19,49 @@ import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.GetMethod;
 
-import com.sujit.utility.Zipper;
+import yjava.security.yca.YCAException;
+import com.sujit.test.ycatest.YcaReader;
+import com.sujit.utility.zipapp.Zipper;
 import com.sujit.yahoo.mailer.SendMail.MailerApp;
 
-import java.util.logging.Logger;
+import org.apache.log4j.Logger;
 
+/**
+ * Class to delete the records from sherpa based on a input file having the key as first column
+ * and any value in the second column. The column separator is "|" symbol.
+ * The output files from execution of this program will be placed in the same folder of the input file.
+ * @author sujitroy
+ *
+ */
 public class App {
 	private final static Logger LOGGER = Logger.getLogger(App.class .getName());
 	@SuppressWarnings("deprecation")
 	public static void main( String[] args ) {
     	String hostNamePart = "";
-    	String headerValue = "";
+    	String ycaAppId = "";
     	String listFilePath = "";
     	if(args.length<3){
-    		LOGGER.severe("Not enough input parameters. Need the query URL string,'Yahoo-App-Auth' header value and source file location (in that order).");
+    		LOGGER.error("Not enough input parameters. Need the query URL string, yahoo yca appid and source file location (in that order).");
     		return;
     	}else{
     		LOGGER.info("Obtained Query URL :"+args[0]);
-    		LOGGER.info("Obtained Header Info :"+args[1]);
+    		LOGGER.info("Obtained YCA AppID :"+args[1]);
     		LOGGER.info("Obtained Source File Path :"+args[2]);
     		hostNamePart = args[0];
-    		headerValue = args[1];
+    		ycaAppId = args[1];
     		listFilePath = args[2];
+    	}
+    	String ycaHeaderValue = "";
+    	try {
+			ycaHeaderValue = YcaReader.getYcaCertificateValue(ycaAppId);
+			LOGGER.info("YCA Header obtained is:"+ycaHeaderValue);
+		} catch (YCAException e1) {
+			LOGGER.error("Error occurred while getting YCA certificate", e1);
+		}
+    	
+    	if(ycaHeaderValue==null || ycaHeaderValue.isEmpty()){
+    		LOGGER.info("Unable to obtain yca app certificate value for the given app-id on this machine. Please check the yca details.");
+    		return;
     	}
     	Scanner scanner = null;
     	BufferedWriter bufferedWriter = null;
@@ -55,7 +76,7 @@ public class App {
      	 outputFilePath = outputFilePath.substring(0, outputFilePath.lastIndexOf(File.separator));
          bufferedWriter = new BufferedWriter(new FileWriter(outputFilePath+File.separator+"SherpaDeleteFailureLog.txt",true));
          resFilePath1 = outputFilePath+File.separator+"SherpaDeleteFailureLog.txt";
-         zipFilePath = outputFilePath+File.separator+"Results.zip";
+         zipFilePath = outputFilePath+File.separator+"DeletionResults.zip";
          Calendar startTime = Calendar.getInstance();
          while ( scanner.hasNextLine() ){
         	  String value = scanner.nextLine();
@@ -65,28 +86,28 @@ public class App {
         	  uuidValue = x[1];
         	  String deleteUrl = hostNamePart+URLEncoder.encode(key);
         	  LOGGER.info("Delete URL - "+deleteUrl);
-        	  int deleteStatus = deleteSherpaEntry(deleteUrl, headerValue);
+        	  int deleteStatus = deleteSherpaEntry(deleteUrl, ycaHeaderValue);
         	  if(deleteStatus==200){
         		  // just chill
         	  }else{
         		  //damn, write the entry in a log file for a retry later
-        		  bufferedWriter.write(key+"|"+uuidValue);
+        		  bufferedWriter.write(key+"|"+uuidValue+"|"+deleteStatus +" - HTTP response from Sherpa");
         		  bufferedWriter.newLine();
         		  bufferedWriter.flush();
         	  }
         	  Calendar flagTime = Calendar.getInstance();
-         	  int status = intervalNotify(startTime, flagTime, "es");
+         	  int status = intervalNotify(startTime, flagTime);
          	  if(status == 1){
          		 startTime = Calendar.getInstance();
          	  }
           }
         } catch (FileNotFoundException e) {
-			LOGGER.severe("File Not Found - "+listFilePath);
+			LOGGER.error("File Not Found - "+listFilePath);
 			e.printStackTrace();
 		} catch (IOException e) {
-			LOGGER.severe("IOException while deleting the stuffs. Writing this instance in log file.");
+			LOGGER.error("IOException while deleting the stuffs. Writing this instance in log file.");
 			try {
-				bufferedWriter.write(key+"|"+uuidValue);
+				bufferedWriter.write(key+"|"+uuidValue+"|"+" - error while deletion: "+e.getMessage());
 				bufferedWriter.newLine();
 			} catch (IOException e1) {
 				e1.printStackTrace();
@@ -100,7 +121,7 @@ public class App {
 	                  bufferedWriter.close();
 	              }
 	          } catch (IOException ex) {
-	        	  LOGGER.severe("Exception while closing the failure log file.");
+	        	  LOGGER.error("Exception while closing the failure log file.");
 	              ex.printStackTrace();
 	          }
 		}
@@ -111,7 +132,7 @@ public class App {
 			Zipper.makeZip(inputFiletoZip, zipFilePath);
 			List<String> fileAttList = new ArrayList<String>();
 			fileAttList.add(zipFilePath);
-			sendMail("Sherpa Delete Script has finished execution. No errors have been reported. All entries in the attached input file has been deleted", "JAKE alt-src key removal from BK1 table", "skhichi@yahoo-inc.com,smeghana@yahoo-inc.com,smanpr@yahoo-inc.com,sakshat@yahoo-inc.com", fileAttList);
+			sendMail("<h3>Sherpa Delete Script has finished execution on soln-stage</h3><br><p>No errors have been reported. All tribune media alt-src entries in the attached input file has been deleted.<br>Attached zip file contains all the TMS alternate keys which were deleted from the table.</p>", "TMS ID alt-src key deletion status from soln-stage", "satyan@yahoo-inc.com,sakshat@yahoo-inc.com", fileAttList);
 		}else{
 			List<String> inputFiletoZip = new ArrayList<String>();
 			inputFiletoZip.add(resFilePath1);
@@ -119,11 +140,16 @@ public class App {
 			Zipper.makeZip(inputFiletoZip, zipFilePath);
 			List<String> fileAttList = new ArrayList<String>();
 			fileAttList.add(zipFilePath);
-			sendMail("Sherpa Delete Script has finished execution. However some errors have been encountered while deleteing entries. Id's which failed to be deleted are available at 'SherpaDeleteFailureLog.txt'", "JAKE alt-src key removal from BK1 table", "skhichi@yahoo-inc.com,smeghana@yahoo-inc.com,smanpr@yahoo-inc.com,sakshat@yahoo-inc.com", fileAttList);
+			sendMail("<h3>Sherpa Delete Script has finished execution on soln-stage</h3><br><p>However some errors have been encountered while deleteing entries. TMS alt-src Key's which failed to be deleted are available at 'SherpaDeleteFailureLog.txt', inside the attached zip file, along with the original file which contains all the TMS ID alt-src keys.</p>", "TMS ID alt-src key deletion status from soln-stage", "satyan@yahoo-inc.com,sakshat@yahoo-inc.com", fileAttList);
 		}
     }
     
-    
+    /**
+     * Method to make the HTTP call to delete a sherpa entry
+     * @param sherpaURL
+     * @param headerValue
+     * @return
+     */
     public static int deleteSherpaEntry(String sherpaURL, String headerValue){
     	int responseData = 0;
     	HttpClient client = new HttpClient();
@@ -143,10 +169,10 @@ public class App {
     		responseData = statusCode;
     	}
     	catch(HttpException e){
-    		LOGGER.severe("Fatal protocol violation: " + e.getMessage());
+    		LOGGER.error("Fatal protocol violation: " + e.getMessage());
     	    e.printStackTrace();
     	}catch(IOException e){
-    		LOGGER.severe("Fatal transport error: " + e.getMessage());
+    		LOGGER.error("Fatal transport error: " + e.getMessage());
     	    e.printStackTrace();
     	}finally{
     		method.releaseConnection();
@@ -154,31 +180,37 @@ public class App {
     	return responseData;
     }
     
+    /**
+     * Common utility method to send mails using the mail send program
+     * @param bodyPart
+     * @param sub
+     * @param cclist
+     * @param fileAttList
+     */
     public static void sendMail(String bodyPart, String sub, String cclist, List<String> fileAttList){
 		try
 	    {
 	      String to="sujitroy@yahoo-inc.com";
 	      String cc = cclist;
-	      String from="sujitroy-autogenerate@yahoo-inc.com";
-	      String subject=sub;
-	      String body=bodyPart+"\n\nDO NOT REPLY TO THIS MESSAGE.";
+	      String from="mailbot@yahoo-inc.com";
+	      String body=bodyPart+"<br><p>Do not reply to this message.</p>";
 	      MailerApp ma = new MailerApp();
-		  ma.sendMail(to, cc, from, subject, body, fileAttList);
+		  ma.sendHTMLMail(to, cc, from, sub, body, fileAttList);
 	    }
 	    catch (Exception ex)
 	    {
-	      ex.printStackTrace();
+	      LOGGER.error("Error while sending mail", ex);
 	    }
 	}
     
-    public static int intervalNotify(Calendar startTime, Calendar flagTime, String regionHeader){
+    public static int intervalNotify(Calendar startTime, Calendar flagTime){
 		int status = 0;
 		long start = startTime.getTimeInMillis();
 		long flag = flagTime.getTimeInMillis();
 		long diff = Math.abs(flag - start);
 		long diffInSeconds = diff/1000;
 		if(diffInSeconds>=900){
-			sendMail("JAKE alt-src key deletion script is still executing for "+regionHeader.toUpperCase()+" region", "JAKE alt-src key deletion script is still executing for "+regionHeader.toUpperCase()+" region", "skhichi@yahoo-inc.com,smeghana@yahoo-inc.com,smanpr@yahoo-inc.com,sakshat@yahoo-inc.com", null);
+			sendMail("<p>TribuneMedia service alt-src key deletion is still going on for soln-stage ID Assigner Sherpa Table</p>", "TMS ID alt-src key deletion status from soln-stage - still going on", "sakshat@yahoo-inc.com", null);
 			status = 1;
 		}
 		return status;
